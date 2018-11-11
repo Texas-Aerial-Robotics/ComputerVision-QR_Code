048fcf90550cb77990c981dc2c9ba7ec2efb9b23
Wilson Watson <wilsonwatson@utexas.edu>
2018-11-10 21:29:02 -0600

    Rectification and parsing complete for br case

--- camera_recog/main.cpp
+++ camera_recog/main.cpp
@@ -1,24 +1,33 @@
-#include <opencv/cv.hpp>
-#include <vector>
-#include <tuple>
 #include <iostream>
+#include <opencv/cv.hpp>
+#include <tuple>
+#include <vector>
+#include <utility>
+
+#include "qr.h"
+
+void onQR(cv::Mat image, std::vector<uint16_t> possibilities){
+  cv::imshow("onqr", image);
+}
 
 int main() {
 
   cv::Mat m, g;
 
-  cv::VideoCapture v(0);
-
+  cv::VideoCapture v("testing_data.mkv");
+  
   while (true) {
 
-    v >> m;
-
+    if (!(v.read(m))) {
+      cv::waitKey(0);
+      break;
+    }
     // cv::cvtColor(m, g, CV_BGR2GRAY);
 
-    cv::inRange(m, cv::Scalar(50, 50, 50), cv::Scalar(255, 255, 255), g);
+    cv::inRange(m, cv::Scalar(50, 1, 1), cv::Scalar(255, 255, 255), g);
 
-    cv::Mat element = getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(9, 9),
-                                            cv::Point(3, 3));
+    cv::Mat element =
+        getStructuringElement(cv::MORPH_RECT, cv::Size(9, 9), cv::Point(3, 3));
 
     cv::dilate(g, g, element);
 
@@ -29,60 +38,110 @@ int main() {
                      CV_CHAIN_APPROX_TC89_L1, cv::Point(0, 0));
 
     cv::Mat drawing = cv::Mat::zeros(g.size(), CV_8UC1);
+    int image_i = 0;
     for (int i = 0; i < contours.size(); i++) {
       cv::Scalar color = cv::Scalar(255, 255, 255);
       cv::drawContours(drawing, contours, i, color, 2, 8, hierarchy, 0,
                        cv::Point());
-    }
-    
-    std::vector<cv::Vec2f> lines; // will hold the results of the detection
-    cv::HoughLines(drawing, lines, 1, CV_PI / 90, 90, 0,
-                   0); // runs the actual detection
-    
-    std::vector<cv::Point> intersections;
-    
-    std::vector<cv::Vec2f> lines_trimmed;
-    
-    for(size_t i = 0; i < lines.size(); i++){
-      float rho = lines[i][0], theta = lines[i][1];
-      double a = cos(theta), b = sin(theta);
-      double x0 = a * rho, y0 = b * rho;
-      bool k = false;
-      for(size_t j = 0; j < lines_trimmed.size(); j++){
-        float rhok = lines_trimmed[j][0], thetak = lines_trimmed[j][1];
-        double a = cos(thetak), b = sin(thetak);
-        double x0k = a * rhok, y0k = b * rhok;
-        double dist = (x0k - x0)*(x0k - x0) + (y0k - y0)*(y0k - y0);
-        if(dist < 5000 && (abs(theta - thetak) < 2 || abs(theta - thetak) > 5)) {
-          k = true;
-          break;
+
+      std::vector<cv::Point> contours_poly;
+      contours_poly.resize(contours[i].size());
+      approxPolyDP(cv::Mat(contours[i]), contours_poly, 15, true);
+      if (contours_poly.size() == 4) {
+        std::vector<cv::Point2f> quad_pts;
+        std::vector<cv::Point2f> squre_pts;
+        quad_pts.push_back(cv::Point2f(contours_poly[0].x, contours_poly[0].y));
+        quad_pts.push_back(cv::Point2f(contours_poly[1].x, contours_poly[1].y));
+        quad_pts.push_back(cv::Point2f(contours_poly[3].x, contours_poly[3].y));
+        quad_pts.push_back(cv::Point2f(contours_poly[2].x, contours_poly[2].y));
+        squre_pts.push_back(cv::Point2f(0, 0));
+        squre_pts.push_back(cv::Point2f(0, g.rows));
+        squre_pts.push_back(cv::Point2f(g.cols, 0));
+        squre_pts.push_back(cv::Point2f(g.cols, g.rows));
+
+        cv::Mat transmtx = getPerspectiveTransform(quad_pts, squre_pts);
+        cv::Mat transformed = cv::Mat::zeros(g.rows, g.cols, CV_8UC1);
+        warpPerspective(g, transformed, transmtx, g.size());
+
+        int histSize = 256;
+        float range[] = {0, 256}; // the upper boundary is exclusive
+        const float *histRange = {range};
+        bool uniform = true, accumulate = false;
+        cv::Mat hist;
+        cv::calcHist(&transformed, 1, 0, cv::Mat(), hist, 1, &histSize,
+                     &histRange, uniform, accumulate);
+        float avg = 0.0;
+        for (int j = 0; j < histSize; j++)
+          avg +=
+              j * hist.at<float>(j - 1) / transformed.rows / transformed.cols;
+        if (avg < 2.0 || avg > 4.0)
+          continue;
+
+        cv::dilate(transformed, transformed, element);
+        cv::erode(transformed, transformed, element);
+        cv::erode(transformed, transformed, element);
+
+        std::stringstream ss;
+        ss << "i" << (image_i++);
+
+        std::vector<std::vector<cv::Point>> innercontours;
+        std::vector<cv::Vec4i> innerhierarchy;
+
+        cv::findContours(transformed, innercontours, innerhierarchy,
+                         cv::RETR_TREE, CV_CHAIN_APPROX_TC89_L1,
+                         cv::Point(0, 0));
+
+        cv::Mat drawingn = cv::Mat::zeros(transformed.size(), CV_8UC3);
+
+        int x1 = -1, x2 = -1, y1 = -1, y2 = -1;
+
+        for (int j = 0; j < innercontours.size(); j++) {
+          cv::Scalar color = cv::Scalar(255, 255, 255);
+          cv::drawContours(drawingn, innercontours, j, color, 2, 8, hierarchy,
+                           0, cv::Point());
+          cv::Rect boundRect = boundingRect(innercontours[j]);
+          if (boundRect.x == 0 && boundRect.y == 0)
+            continue;
+          if (x1 == -1) {
+            x1 = boundRect.x;
+            y1 = boundRect.y;
+            x2 = x1 + boundRect.width;
+            y2 = y1 + boundRect.height;
+          } else {
+            x1 = (x1 < boundRect.x) ? x1 : boundRect.x;
+            y1 = (y1 < boundRect.y) ? y1 : boundRect.y;
+            int _x2 = boundRect.x + boundRect.width;
+            int _y2 = boundRect.y + boundRect.height;
+            x2 = (x2 > _x2) ? x2 : _x2;
+            y2 = (y2 > _y2) ? y2 : _y2;
+          }
         }
-      }
-      if(k) continue;
-      lines_trimmed.push_back(lines[i]);
-    }
-    
-    // Draw the lines
-    for (size_t i = 0; i < lines_trimmed.size(); i++) {
-      for(size_t j = i + 1; j < lines_trimmed.size(); j++){
-        auto standard = [&](int m){
-          float rho = lines_trimmed[m][0], theta = lines_trimmed[m][1];
-          double a = cos(theta), b = sin(theta);
-          double x0 = a * rho, y0 = b * rho;
-          double x1 = x0 + 1000 * (-b);
-          double y1 = y0 + 1000 * (a);
-          double x2 = x0 - 1000 * (-b);
-          double y2 = y0 - 1000 * (a);
-          return std::tuple<double, double, double>(y1 - y2, x2 - x1, (x1 - x2) * y1 + (y2 - y1) * x1);
-        };
-        auto [a1, b1, c1] = standard(i);
-        auto [a2, b2, c2] = standard(j);
-        double det = a1 * b2 - a2 * b1;
-        if(det == 0) continue; // parallel lines
-        double x = -(c1 * b2 - c2 * b1) / det;
-        double y = -(a1 * c2 - a2 * c1) / det;
-        std::cout << x << "," << y << std::endl;
-        cv::circle(m, cv::Point(x, y), 3, cv::Scalar(0, 128, 255), 3);
+
+        double score = fabs(1 - (double)(x2 - x1) / (double)(y2 - y1));
+        std::stringstream s2;
+
+        if (score > 0.5 || score != score)
+          continue; // score != score is true if score is NaN
+
+        double stride = ((x2 - x1) > (y2 - y1) ? (x2 - x1) : (y2 - y1)) / 11.0;
+        
+        qr_comb_t qr(11);
+        
+        for (int c = 0; c < 11; c++) {
+          for (int r = 0; r < 11; r++) {
+            cv::Point p(x1 + (int)(stride * r + 0.5 * stride),
+                        y1 + (int)(stride * c + 0.5 * stride));
+            cv::circle(drawingn, p, 5, cv::Scalar(0, 0, 255), 5);
+            auto v = transformed.at<uchar>(p);
+            qr(r, c) = v > 128 ? white : black;
+          }
+        }
+        
+        std::vector<uint16_t> values;
+        
+        qr.compute(values);
+        
+        onQR(transformed, values);
       }
     }
 
@@ -93,10 +152,8 @@ int main() {
       break;
     }
   }
+  
+  fflush(stdout);
 
   return 0;
 }
-
-
-
-
---
+++ camera_recog/qr_br.cpp
@@ -0,0 +1,170 @@
+#include "qr.h"
+#include <functional>
+
+std::vector<std::function<bool(int, int)>> masks = {
+    [](int i, int j) -> bool { return j % 3 == 0; },
+    [](int i, int j) -> bool { return (i + j) % 3 == 0; },
+    [](int i, int j) -> bool { return (i + j) % 2 == 0; },
+    [](int i, int j) -> bool { return i % 2 == 0; },
+    [](int i, int j) -> bool { return ((i * j) % 3 + i * j) % 2 == 0; },
+    [](int i, int j) -> bool { return ((i * j) % 3 + i + j) % 2 == 0; },
+    [](int i, int j) -> bool { return (i / 2 + j / 3) % 2 == 0; },
+    [](int i, int j) -> bool { return (i * j) % 2 + (i * j) % 3 == 0; }};
+
+uint8_t mask_v(int i, int j, int m) { return masks[m](i, j) ? 1 : 0; }
+
+std::tuple<int, int> index_rotate(int i, int j, int width, int ninety) {
+  if (ninety % 4 == 0) {
+    return std::make_tuple(i, j);
+  } else if (ninety % 4 == 1) {
+    return std::make_tuple(j, width - i - 1); // rotate 90
+  } else if (ninety % 4 == 2) {
+    return std::make_tuple(width - i - 1, width - j - 1); // rotate 180
+  }
+  return std::make_tuple(width - j - 1, i); // rotate 270
+}
+
+// extern void cuda_br(std::array<uint8_t, 18>&, int);
+
+void cpu_br(std::array<uint8_t, 18> &data, int rotate, std::vector<uint16_t>&);
+
+void precomp_br(int width, qr_comb_t *comb, std::vector<uint16_t>& values) {
+  std::vector<std::tuple<int, int>> rotate_mask_pairs;
+  for (int k = 0; k < 4; k++) {
+    for (int m = 0; m < masks.size(); m++) {
+
+      // Get encoding
+      uint8_t enc = 0;
+      for (int z = 0; z < 4; z++) {
+        enc <<= 1;
+        auto [i, j] = index_rotate(z % 2, z / 2, width, k + 2);
+        uint8_t t =
+            (comb->extract(j, i) ^ mask_v(i + width - 1, j + width - 1, m)) & 1;
+        enc += t;
+      }
+      if (enc == 1) {
+        rotate_mask_pairs.push_back(std::make_tuple(k, m));
+      }
+    }
+  }
+  for (int i = 0; i < rotate_mask_pairs.size(); i++) {
+    int rotate = std::get<0>(rotate_mask_pairs[i]);
+    int mask = std::get<1>(rotate_mask_pairs[i]);
+
+    auto masked_bit = [&](int i_, int j_) {
+      auto [i, j] = index_rotate(i_, j_, width, rotate + 2);
+      return (comb->extract(j, i) ^
+              mask_v(i_ + width - 1, j_ + width - 1, mask)) &
+             1;
+    };
+
+    auto read_codeword_up = [&](int _i, int _j) -> uint8_t {
+      if (_i == 0 && _j == 0)
+        ;
+      uint8_t cw = 0;
+      for (int y = 0; y < 4; y++) {
+        for (int x = 0; x < 2; x++) {
+          cw <<= 1;
+          auto [i, j] = index_rotate(x + _i, y + _j, width, rotate + 2);
+          if (i < 0 || j < 0)
+            continue;
+          auto t = masked_bit(y + _j, x + _i);
+          cw += t;
+        }
+      }
+      return cw;
+    };
+    auto read_codeword_down = [&](int _i, int _j) -> uint8_t {
+      uint8_t cw = 0;
+      for (int y = 3; y >= 0; y--) {
+        for (int x = 0; x < 2; x++) {
+          cw <<= 1;
+          auto t = masked_bit(y + _j, x + _i);
+          cw += t;
+        }
+      }
+      return cw;
+    };
+    std::array<uint8_t, 18> codewords = {
+        read_codeword_up(0, 0),    read_codeword_up(0, 4),
+        read_codeword_up(0, 8),   // missing bits 1 and 2
+        read_codeword_down(2, 8), // missing bits 128 and 64
+        read_codeword_down(2, 4),  read_codeword_down(2, 0),
+        read_codeword_up(4, 0),    read_codeword_up(4, 4),
+        read_codeword_up(4, 8),   // missing bits 1 and 2
+        read_codeword_down(6, 8), // missing bits 128 and 64
+        read_codeword_down(6, 4),  read_codeword_down(6, 0),
+        read_codeword_up(8, 0),    read_codeword_up(8, 4),
+        read_codeword_up(8, 8),    // missing bits 1 and 2
+        read_codeword_down(10, 8), // missing alot
+        read_codeword_down(10, 4), // missing alot
+        read_codeword_down(10, 0)  // missing alot
+    };
+    cpu_br(codewords, width, values);
+  }
+}
+
+template <int i>
+int extract(const std::array<uint8_t, i> &bytes, int pos, int len) {
+  int shift = 24 - (pos & 7) - len;
+  int mask = (1 << len) - 1;
+  int byteIndex = pos >> 3;
+  return (((bytes[byteIndex] << 16) | (bytes[++byteIndex] << 8) |
+           bytes[++byteIndex]) >>
+          shift) &
+         mask;
+}
+
+std::pair<bool, uint16_t> extract_numeric(const std::array<uint8_t, 14> &bytes,
+                                          int bit_idx) {
+  int n = extract<14>(bytes, bit_idx, 10);
+  if (n != 4)
+    return std::make_pair(false, n);
+  bit_idx += 10;
+  int x = extract<14>(bytes, bit_idx, 10);
+  bit_idx += 10;
+  uint16_t k = x;
+  if(k >= 1000) 
+    return std::make_pair(false, k);
+  x = extract<14>(bytes, bit_idx, 4);
+  bit_idx += 4;
+  if (x >= 10)
+    return std::make_pair(false, x);
+  uint8_t d = x;
+  return std::make_pair(true, k * 10 + d);
+}
+
+void compute_num(const std::array<uint8_t, 18> &data, uint8_t changes,
+                 std::vector<uint16_t> &answers) {
+  std::array<uint8_t, 14> working;
+  for (int i = 0; i < 14; i++) {
+    working[i] = data[i];
+  }
+
+  working[2] |= ((changes) & (1)) << 1;
+  working[2] |= ((changes >> (1)) & 1);
+  working[3] |= ((changes >> (2)) & 1) << 7;
+  working[3] |= ((changes >> (3)) & 1) << 6;
+
+  int bit_idx = 4;
+  int mode = extract<14>(working, 0, 4);
+  if (mode != 1)
+    return;
+
+  auto [yeah, x] = extract_numeric(working, bit_idx);
+  if (yeah) {
+    for (int i = 0; i < answers.size(); i++) {
+      if (answers[i] == x)
+        return;
+    }
+    answers.push_back(x); // TODO yeah, its not working
+  }
+}
+
+void cpu_br(std::array<uint8_t, 18> &data, int width, std::vector<uint16_t>& values) {
+  for (uint8_t i = 0; i <= 0x0f; i++) {
+    compute_num(data, i, values);
+  }
+}
+
+void qr_comb_t::compute(std::vector<uint16_t>& values) { precomp_br(width, this, values); }
