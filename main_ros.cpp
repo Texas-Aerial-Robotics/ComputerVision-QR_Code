ed4782e594640e545f147bff0c8cbf896ed73960
Wilson Watson <wilsonwatson@utexas.edu>
2019-04-13 13:11:59 -0500

    Added ROS node

--- main_ros.cpp
+++ main_ros.cpp
@@ -2,6 +2,7 @@
 
 #include <ros/ros.h>
 #include <std_msgs/String.h>
+#include <std_msgs/UInt32.h>
 #include <iostream>
 #include <string>
 #include <assert.h>
@@ -15,29 +16,28 @@ int main(int argc, char* argv[]) {
   std::string video_stream_provider;
   cv::VideoCapture vc;
   if (_nh.getParam("video_stream_provider", video_stream_provider)){
-      ROS_INFO_STREAM("Resource video_stream_provider: " << video_stream_provider);
-      // If we are given a string of 4 chars or less (I don't think we'll have more than 100 video devices connected)
-      // treat is as a number and act accordingly so we open up the videoNUMBER device
-      if (video_stream_provider.size() < 4){
-          ROS_INFO_STREAM("Getting video from provider: /dev/video" << video_stream_provider);
-          vc.open(atoi(video_stream_provider.c_str()));
-      }
-      else{
-          ROS_INFO_STREAM("Getting video from provider: " << video_stream_provider);
-
-          std::cout << "Using compressed " << std::endl;
-          vc.open(video_stream_provider);
-          vc.set(CV_CAP_PROP_FOURCC ,CV_FOURCC('M', 'J', 'P', 'G') );
-          vc.set(CV_CAP_PROP_FRAME_WIDTH ,640);
-          vc.set(CV_CAP_PROP_FRAME_HEIGHT ,480);
-      }
+    ROS_INFO_STREAM("Resource video_stream_provider: " << video_stream_provider);
+    // If we are given a string of 4 chars or less (I don't think we'll have more than 100 video devices connected)
+    // treat is as a number and act accordingly so we open up the videoNUMBER device
+    if (video_stream_provider.size() < 4){
+      ROS_INFO_STREAM("Getting video from provider: /dev/video" << video_stream_provider);
+      vc.open(atoi(video_stream_provider.c_str()));
+    } else{
+      ROS_INFO_STREAM("Getting video from provider: " << video_stream_provider);
+      std::cout << "Using compressed " << std::endl;
+      vc.open(video_stream_provider);
+      vc.set(CV_CAP_PROP_FOURCC ,CV_FOURCC('M', 'J', 'P', 'G') );
+      vc.set(CV_CAP_PROP_FRAME_WIDTH ,640);
+      vc.set(CV_CAP_PROP_FRAME_HEIGHT ,480);
+    }
   }
   else{
-      ROS_ERROR("Failed to get param 'video_stream_provider'");
-      return -1;
+    ROS_ERROR("Failed to get param 'video_stream_provider'");
+    return -1;
   }
 
   ros::Publisher pub = nh.advertise<std_msgs::String>("output", 5);
+  ros::Publisher num = nh.advertise<std_msgs::UInt32>("", 1);
 
   cv::Mat m;
 
